54f9eca0f8cc14514c78f70bce63080eda27f88d
Omar Kassem <omarkassem099@gmail.com>
2024-12-25 16:45:44 +0200

    Feat(add sdk utls

---
+++ scripts/publish/scrpublishPackage.ts
@@ -0,0 +1,47 @@
+import { execSync } from "child_process";
+import { constants } from "../constants";
+import { updateConstants } from "../utils";
+import * as fs from "fs";
+import path from "path";
+
+
+const publish = () => {
+  const network = constants["network"];
+  execSync(`bash publish.sh ${network}`, { stdio: 'inherit' });
+}
+
+const parsePublishJson = () => {
+  console.log(__dirname)
+  const raw = fs.readFileSync(path.join(__dirname, "publish.json"));
+  const result: any = JSON.parse(raw.toString());
+  if (!result.digest) {
+    throw new Error("Make sure to run publish.sh first");
+  }
+
+  result.objectChanges.forEach((obj: any) => {
+    console.log
+    if (obj.type === "published") {
+      constants["packageId"] = obj.packageId;
+    } else if (obj.type === "created") {
+      if (obj.objectType.includes("Publisher")) {
+        constants["publisher"] = obj.objectId;
+      }
+      if (obj.objectType.includes("AdminCap")) {
+        constants["adminCap"] = obj.objectId;
+      }
+      if (obj.objectType.includes("Central")) {
+        constants["centralObj"] = obj.objectId;
+      }
+      if (obj.objectType.includes("Kiosk")) {
+        constants["kiosk"] = obj.objectId;
+      }
+    }
+  });
+
+};
+
+
+// Here one can add more logic, whether to publish each time or not, etc...
+// publish();
+parsePublishJson();
+updateConstants(constants);
